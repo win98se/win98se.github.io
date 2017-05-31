9fc13feca15389663111c5df19781ed886d0b0b7
Lim Chunwei <win98se@rocketmail.com>
2017-06-01 02:02:53 +0800

    MS OS EOS pages rework

---
+++ msoseos/header.js
@@ -0,0 +1,6 @@
+document.getElementById("menu").innerHTML="<p><a href=\"index.htm\">Client products</a></p>\
+<p><a href=\"server.htm\">Server products</a></p>\
+<p><a href=\"#\">Ancient products</a></p>\
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_9x.htm\">MS-DOS & Windows 9x</a></p>\
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_nt.htm\">Windows NT</a></p>\
+";
