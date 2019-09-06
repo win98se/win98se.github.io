ae2709ab85c39317837c139dca46661cc77faf6c
Lim Chunwei <win98se@users.noreply.github.com>
2019-09-06 23:30:23 +0800

    Remove ch8newssg live page and update pdate MS OS EOS pages
    I gave up.

--- msoseos/header.js
+++ msoseos/header.js
@@ -2,5 +2,7 @@ document.getElementById("menu").innerHTML="<p><a href=\"index.htm\">Client produ
 <p><a href=\"server.htm\">Server products</a></p>\
 <p><a href=\"#\">Ancient products</a></p>\
 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_9x.htm\">MS-DOS & Windows 9x</a></p>\
-<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_nt.htm\">Windows NT</a></p>\
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_nt.htm\">Windows NT (except Windows 10)</a></p>\
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_10_sac.htm\">Windows 10 (Semi-Annual Channel)</a></p>\
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_10_ltsc.htm\">Windows 10 (Long-Term Servicing Channel)</a></p>\
 ";
