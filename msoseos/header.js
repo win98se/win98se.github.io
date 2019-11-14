e20fbe0283701cd9954206acea765b41d684fa1e
Lim Chunwei <win98se@users.noreply.github.com>
2019-11-14 13:34:32 +0800

    Update MS OS EOS pages
    Seems like I was sleeping during the last commit

--- msoseos/header.js
+++ msoseos/header.js
@@ -1,6 +1,6 @@
 document.getElementById("menu").innerHTML="<p><a href=\"index.htm\">Client products</a></p>\
 <p><a href=\"server.htm\">Server products</a></p>\
-<p><a href=\"#\">Ancient products</a></p>\
+<p>Ancient products</p>\
 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_9x.htm\">MS-DOS & Windows 9x</a></p>\
 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_nt.htm\">Windows NT (except Windows 10)</a></p>\
 <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"ancient_10_sac.htm\">Windows 10 (Semi-Annual Channel)</a></p>\
